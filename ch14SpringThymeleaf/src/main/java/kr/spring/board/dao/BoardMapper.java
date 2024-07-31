b4fd791f21dc40fc95e8d2da0b51eaed3af37f4c
SongHyogi <thdhk1230@gmail.com>
2024-07-31 16:22:41 +0900

    ch14_SpringThymeleaf

---
+++ ch14SpringThymeleaf/src/main/java/kr/spring/board/dao/BoardMapper.java
@@ -0,0 +1,25 @@
+package kr.spring.board.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import kr.spring.board.vo.BoardVO;
+
+@Mapper
+public interface BoardMapper {
+	public void insertBoard(BoardVO board);
+	@Select("SELECT COUNT(*) FROM aboard")
+	public int getBoardCount();
+	public List<BoardVO> getBoardList(Map<String,Integer> map);
+	@Select("SELECT * FROM aboard WHERE num=#{num}")
+	public BoardVO getBoard(int num);
+	@Update("UPDATE aboard SET writer=#{writer},title=#{title},content=#{content} WHERE num=#{num}")
+	public void updateBoard(BoardVO board);
+	@Delete("DELETE FROM aboard WHERE num=#{num}")
+	public void deleteBoard(int num);
+}
